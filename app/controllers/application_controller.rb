1ff1b279cc0ccd046a7a5037718a7dcd16d046d0
ssoonsl <ssoonsl616+gh@gmail.com>
2017-07-05 09:53:46 +0800

    Add user authentication through username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,9 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    added_attrs = [:username, :email, :first_name, :last_name, :password, :password_confirmation, :remember_me, :avatar, :avatar_cache, :remove_avatar]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
 
 end
